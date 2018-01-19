968890c96bbabf5b81bd5a0c76c4d4e1d0c0be7c
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-19 21:22:14 +0300

    изучил дополнительные функции оператора let

--- src/let/let.js
+++ src/let/let.js
@@ -1,9 +1,23 @@
-function name() {
-  if (true) {
-    let a = 1;
-  }
-  let a = 2;
-  console.log(a);
+// function name() {
+//   if (true) {
+//     let a = 1;
+//   }
+//   let a = 2;
+//   console.log(a); //2
+// }
+
+// name();
+
+let fs = [];
+
+for(let i=0; i<10; i++) {
+  fs.push(function() {
+    console.log(i);
+  })
 }
 
-name();
+fs[0](); //0
+fs[3](); //3
+
+//переменная let доступна только внутри блока с функциями
+//переменная let не определяется повторно
