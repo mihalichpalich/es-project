831a27857f05460a9d369c6729ab8ac07b3cfc6a
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-20 00:11:10 +0300

    изучил spread

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ var [first, , , , fifth] = ['red', 'yellow', 'green', 'blue', 'orange'];
 //console.log(first, fifth);
 
 function getSizes({width = 50, height = 60}) {
-  console.log(width, height);
+  //console.log(width, height);
 }
 
 getSizes({
@@ -84,3 +84,34 @@ getSizes({
 //можно переопределять значения свойств объекта, если они не заданы по умолчанию
 //также деструктуризацию можно применять и к массивам и функциям
 //если доступ нужен к определенным значениям, остальные можно не указывать
+
+
+//spread
+
+// const getText = (a,...args) => {
+//   //console.log(a, args);
+// }
+
+//getText('a', 'b', 'c');
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+arr1.push(...arr2);
+//console.log(arr1);
+
+const args = ['a', 'b', 'c'];
+
+const getText = (a, b, c) => {
+  console.log(a, b, c);
+}
+
+getText(...args);
+
+//args - оператор spread, массив аргументов функции или значений массива
+//spread в функциях удобен когда количество аргументов неизвестно
+//с помощью spread можно переносить значения из одного массива в другой
+//а также передавать массив в качестве параметров
+
+
+
