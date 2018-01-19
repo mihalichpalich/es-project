d8a2bd2ec212c1435e581f479addff0c2667a3a1
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-19 21:01:21 +0300

    изучил стрелочные функции

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-let a = 1;
-let b = 2;
+//стрелочные функции
+
+var getTrack = (...args) => {
+  //console.log(args);
+  return args[0] + ' ' + args[1];
+}
+
+//console.log(getTrack("Artist", "Track"));
+
+// function API() {
+//   this.url = 'http://google.com';
+// }
+
+// API.prototype.get = function() {
+//   return [1,2,3].map(number => {
+//     console.log('this', this);
+//     return number;
+//   })
+// }
+
+// var api = new API();
+// api.get();
+
+//у стрелочных функций нет контекста
