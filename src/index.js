e86ea41aed458e703b6e7b0e79d2f86f2e91a01b
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-21 21:53:51 +0300

    изучил js-классы

--- src/index.js
+++ src/index.js
@@ -143,4 +143,45 @@ const tag = (strings, ...values) => {
 
 const message = tag`Its ${new Date().getHours()} and Im ${'sleepy'}`;
 
-console.log(message);
+//console.log(message);
+
+
+//классы
+
+class SimpleDate {
+  constructor(year, month, day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  toString() {
+    return `${this.day}/${this.month}/${this.year}`;
+  }
+}
+
+const today = new SimpleDate(2017, 1, 28);
+
+//console.log(today.toString());
+
+//наследование
+class OtherDate extends SimpleDate {
+  constructor(year, month, day) {
+    super(year, month, day); //обращение к родительскому классу
+  }
+
+  static hello() {
+    //console.log('hello OtherDate');
+  }
+
+  toString() {
+    return `${this.day}.${this.month}.${this.year}`;
+  }
+}
+
+const tomorrow = new OtherDate(2017, 1, 29);
+//console.log(tomorrow.toString());
+
+OtherDate.hello();
+
+//класс не вызывается как функция
