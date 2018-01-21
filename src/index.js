ac6c92e06f1b85dc177783c61ff060cab7fb183b
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-21 22:36:02 +0300

    изучил промисы

--- src/index.js
+++ src/index.js
@@ -185,3 +185,30 @@ const tomorrow = new OtherDate(2017, 1, 29);
 OtherDate.hello();
 
 //класс не вызывается как функция
+
+
+//promises
+
+const promise = new Promise((resolve, reject) => {
+  //throw new Error('Alarm');
+
+  setTimeout(() => {
+    if (true) {
+      resolve('hello world');
+    } else {
+      reject();
+    }
+  }, 2000);
+});
+
+// promise.then(
+//   data => console.log('success: ', data),
+//   error => console.log('error: ', error)
+// );
+  // .then(
+  //   data => console.log('success2: ', data),
+  //   error => console.log('error: ', error)
+  // )
+  //.catch(error => console.log('error: ', error));
+
+
