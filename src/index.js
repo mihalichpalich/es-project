5ff5587843a380a2464d48622b5a4712e4ef15aa
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-22 15:12:05 +0300

    изучил цикл for...of

--- src/index.js
+++ src/index.js
@@ -254,5 +254,24 @@ const getProducts = async() => {
 }
 
 getProducts().then(result => {
-  console.log('result', result);
+  //console.log('result', result);
 });
+
+
+//for..of
+
+let browsers = ['Chrome', 'Firefox', 'Edge', 'Safari', 'Opera'];
+
+// for (let browser in browsers) {
+//   console.log(browser); //0 1 2 3 4
+// }
+
+// for (let index in browsers) {
+//   console.log(browsers[index]);
+// }
+
+for (let browser of browsers) {
+  console.log(browser);
+}
+
+console.log(browsers);
