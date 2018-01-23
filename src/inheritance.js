0a27e98d761a1896168b4e468260ccc12d57d37e
mihalichpalich <pavlovtfspb@gmail.com>
2018-01-23 10:55:50 +0300

    изучил наследование

---
+++ src/inheritance.js
@@ -0,0 +1,51 @@
+class Task {
+  constructor(title) {
+    this.title = title;
+    this.done = false;
+    Task.count += 1;
+    console.log('Создание задачи');
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(value) {
+    this._title = value;
+  }
+
+  static getDefaultTitle() {
+    return 'Задача';
+  }
+
+  complete() {
+    this.done = true;
+    console.log(`Задача "${this.title}" выполнена`);
+  }
+}
+
+Task.count = 0;
+
+class SubTask extends Task {
+  constructor(title, parent) {
+    super(title);
+    this.parent = parent;
+    console.log('Создание подзадачи');
+  }
+
+  complete() {
+    super.complete();
+    console.log(`Подзадача "${this.title}" выполнена`);
+  }
+}
+
+let task = new Task('Изучить JavaScript');
+let subTask = new SubTask('Изучить ES6', task);
+
+console.log(SubTask.getDefaultTitle());
+console.log(SubTask.count);
+
+task.complete();
+subTask.complete();
+
+console.log(task, subTask);
